f9e9e1cf3dc0a5352d068a96d78ae2d9de61a8fa
Gourav230705 <152266930+Gourav230705@users.noreply.github.com>
2025-07-18 13:12:15 +0530

    Add files via upload
    added transaction form and dashboard to list all data

---
+++ Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TransactionList from './TransactionList'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>This is the dashboard</h1>
+      <TransactionList/>
+    </div>
+  )
+}
