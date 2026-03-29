020c1df8b350705a2a5d3fe40bcde61e7c152725
Himanshu Rajput <aditya84961@gmail.com>
2026-03-29 23:31:04 +0530

    .

--- backend/routes/transactionRouter.js
+++ backend/routes/transactionRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const usersController = require("../controllers/usersCtrl");
 const isAuthenticated = require("../middlewares/isAuth");
 const categoryController = require("../controllers/categoryCtrl");
-const transactionController = require("../controllers/TransactionCtrl");
+const transactionController = require("../controllers/transactionCtrl");
 const transactionRouter = express.Router();
 //add
 transactionRouter.post("/api/v1/transaction/create",isAuthenticated ,transactionController.create);
